d73cbea7777498bbaa49d275c31a0cca76578a29
Ethan Burda <burdaethan3@gmail.com>
2018-09-05 12:02:53 -0700

    StringBuilder Exercise
    This is an excercise for StringBuilder in C# console app.

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "michael ";
+            string food = "likes to eat steaks on ";
+            string day = "thursday.";
+            name = name.ToUpper();
+            Console.WriteLine(name + food + day);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Who eats steaks on thurday?");
+            sb.AppendLine("Michael does!");
+            sb.Append("I could eat steaks anyday of the week!");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}

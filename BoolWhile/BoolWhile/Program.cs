02f963b15a9f2b562032952081908992201af049
Ethan Burda <burdaethan3@gmail.com>
2018-09-05 07:49:31 -0700

    Bool While statment
    This is a boolean comparison using a while statment in C# console app.

---
+++ BoolWhile/BoolWhile/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my favorite number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 3;
+
+            while (!isGuessed)
+            {
+                switch (number)
+                {
+                    case 49:
+                        Console.WriteLine("You guessed 49. Try again.");
+                        Console.WriteLine("Guess a number.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 23:
+                        Console.WriteLine("You guessed 23. Try again.");
+                        Console.WriteLine("Guess a number.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 82:
+                        Console.WriteLine("You guessed 82. Try again.");
+                        Console.WriteLine("Guess a number.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 3:
+                        Console.WriteLine("You picked 3, the best number! Congratulations!");
+                        isGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You are incorrect.");
+                        Console.WriteLine("Guess a number.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            Console.Read();
+        }
+    }
+}
